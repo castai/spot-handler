3edc6b05f7785961311d458a11e0c14e492bafc5
Tadeuš Varnas <varnastadeus@gmail.com>
2022-04-14 14:07:17 +0300

    gke: handle maintenance events

--- handler/gcp_test.go
+++ handler/gcp_test.go
@@ -2,30 +2,28 @@ package handler
 
 import (
 	"context"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
-	"github.com/go-resty/resty/v2"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGCPInterruptChecker(t *testing.T) {
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		require.Equal(t, "/computeMetadata/v1/instance/preempted", r.URL.String())
-
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write([]byte("TRUE"))
-		require.NoError(t, err)
-	}))
-	defer s.Close()
-
 	checker := gcpInterruptChecker{
-		client:            resty.New(),
-		metadataServerURL: s.URL,
+		metadata: mockMetadata{},
 	}
 
 	interrupted, err := checker.Check(context.Background())
 	require.NoError(t, err)
 	require.True(t, interrupted)
+}
+
+type mockMetadata struct {
+}
+
+func (md mockMetadata) Get(path string) (string, error) {
+	m := map[string]string{
+		"instance/maintenance-event": "NO_MAINTENANCE",
+		"instance/preempted":         "TRUE",
+	}
+	return m[path], nil
 }
