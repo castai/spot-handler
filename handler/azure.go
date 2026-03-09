f4b4c699acd0d691d297fed1b90b397e94fb9108
Lachezar Tsonov <lachezar@cast.ai>
2026-03-09 15:39:51 +0200

    Extend logging per incarnation and more event details

--- handler/azure.go
+++ handler/azure.go
@@ -29,11 +29,19 @@ type azureInterruptChecker struct {
 	log               logrus.FieldLogger
 }
 
+// azureSpotScheduledEvent is a single event schema, not all fields are necessarily mapped
+// see https://learn.microsoft.com/en-us/azure/virtual-machines/linux/scheduled-events#the-basics for details
 type azureSpotScheduledEvent struct {
-	EventType string
+	EventId     string
+	EventType   string
+	EventStatus string
+	EventSource string
+	Description string
+	NotBefore   string
 }
 type azureSpotScheduledEvents struct {
-	Events []azureSpotScheduledEvent
+	DocumentIncarnation int
+	Events              []azureSpotScheduledEvent
 }
 
 func (c *azureInterruptChecker) CheckInterrupt(ctx context.Context) (bool, error) {
@@ -50,7 +58,15 @@ func (c *azureInterruptChecker) CheckInterrupt(ctx context.Context) (bool, error
 		return false, fmt.Errorf("received unexpected status code: %d", resp.StatusCode())
 	}
 
+	if len(responseBody.Events) == 0 {
+		return false, nil
+	}
+
+	c.log.Debugf("Received %d scheduled events with incarnation %d", len(responseBody.Events), responseBody.DocumentIncarnation)
+
 	for _, e := range responseBody.Events {
+		c.log.Debugf("Scheduled event seen: EventId=%s, EventType=%s, EventStatus=%s, EventSource=%s, NotBefore=%s, Description=%s",
+			e.EventId, e.EventType, e.EventStatus, e.EventSource, e.NotBefore, e.Description)
 		if e.EventType == "Preempt" {
 			return true, nil
 		}
